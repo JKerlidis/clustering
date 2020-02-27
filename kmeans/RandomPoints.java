165074f4bd59b989a78015e9d876996408ff5f8b
JKerlidis <jamesk@ozemail.com.au>
2020-02-27 16:30:44 +1100

    added console output of randomly generated cluster centres

--- kmeans/RandomPoints.java
+++ kmeans/RandomPoints.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class RandomPoints {
   static ArrayList<Double[]> numbers = new ArrayList<>();
 
+  // Define static constants
   static int NUM_CLUSTERS = 3;
   static int CLUSTER_DIMENSION = 3;
   static int CLUSTER_CENTRE_RANGE = 15;
@@ -11,16 +12,24 @@ public class RandomPoints {
   static int NUM_POINTS = 60;
 
   public static void main(String[] args) {
+    // Set cluster centres based on the above constants
     Random rand = new Random();
     Double[][] clusterCentres = new Double[NUM_CLUSTERS][CLUSTER_DIMENSION];
+
+    System.out.printf("There are %d %d-dimensional clusters with centres: %n",
+      NUM_CLUSTERS, CLUSTER_DIMENSION);
+    System.out.println();
 
     for (int clustNum = 0; clustNum < NUM_CLUSTERS; clustNum++) {
       for (int clustDim = 0; clustDim < CLUSTER_DIMENSION; clustDim++) {
         clusterCentres[clustNum][clustDim] = rand.nextInt(CLUSTER_CENTRE_RANGE)
           + CLUSTER_CENTRE_MIN + rand.nextDouble();
       }
+
+      System.out.println(Arrays.toString(clusterCentres[clustNum]));
     }
 
+    // Generate points around one of the cluster centres
     for (int i = 0; i < NUM_POINTS; i++) {
       Double[] pointValues = new Double[CLUSTER_DIMENSION];
       int clusterNum = rand.nextInt(NUM_CLUSTERS);
@@ -34,6 +43,7 @@ public class RandomPoints {
       numbers.add(pointValues);
     }
 
+    // Write data to a file as comma-separated values
     try {
       BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
         new FileOutputStream("points.txt"), "utf-8"));
